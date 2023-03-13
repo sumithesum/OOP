b63bec921ccd76e87578d68c887b0baa0b7e3554
sumithesum <sumurduc2003@gmail.com>
2023-03-13 14:42:39 +0200

    Update-2

--- Tema1/main.cpp
+++ Tema1/main.cpp
@@ -9,10 +9,14 @@ private:
     int pret;
     char *name;
 public :
-     joc (int pret , char *name,float rating,int cantititate){
+     joc (int pret , char *name,float rating,int cantitate){
         this->pret = pret;
         this->name = new char[strlen(name)+1];
         strcpy(this->name,name);
+        if (rating > 10)
+            rating = 10;
+        if (rating < 0)
+            rating = 0;
         this->rating = rating;
         this->cantitate = cantitate;
 }
@@ -49,11 +53,15 @@ public :
     void setPret(const int pret){
         this->pret = pret;
 }
-    void setRating(const float rating){
-        this->pret = pret;
+    void setRating(float rating){
+        if (rating > 10.0)
+            rating = 10.0;
+        if (rating < 0.0)
+            rating = 0.0;
+        this->rating = rating;
     }
-    void setCantitiate(const int cantitate){
-        this->pret = pret;
+    void setCantitiate(const unsigned int cantitate){
+        this->cantitate = cantitate;
     }
 
     void operator= (const joc &s) {
@@ -76,6 +84,44 @@ public :
         if (!(s.pret == this->pret && strcmp(this->name, s.name) == 0 && this->rating == s.rating && this->cantitate == s.cantitate))
             return 1;
         return 0;
+    }
+    friend std::istream& operator>>(std::istream &is, joc &j) {
+
+        char buf[100];
+        is >> buf;
+        j.setName(buf);
+
+        is >> j.pret>>j.cantitate>>j.rating>>j.cantitate;
+
+        return is;
+    }
+    friend std::ostream& operator<<(std::ostream &os, joc &s) {
+
+        if (!s.name) {
+            os << "Not initialized\n";
+            return os;
+        }
+
+        os << "Student: name: " << s.name << " pret: " << s.pret << " rating: " << s.rating <<" cantitate:" <<s.cantitate <<  '\n';
+
+        return os;
+    }
+    friend class magazin;
+
+};
+
+class Magazin{
+private:
+     unsigned  int nrJocuri;
+     joc** listaJocuri = new joc*[nrJocuri*sizeof(joc*)];
+public:
+    Magazin(unsigned  int nrJocuri, joc **listaJocuri){
+            this->nrJocuri = nrJocuri;
+            for(int i = 0;i <nrJocuri;i++)
+                this->listaJocuri[i] = listaJocuri[i];
+    }
+    ~Magazin(){
+        delete[] listaJocuri;
     }
 };
 
