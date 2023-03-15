ac68b421dbf48990af4f10c8390332a23cd73eab
sumithesum <sumurduc2003@gmail.com>
2023-03-15 03:39:40 +0200

    Update-4

--- Tema1/main.cpp
+++ Tema1/main.cpp
@@ -97,8 +97,11 @@ public :
         char buf[100];
         is >> buf;
         j.setName(buf);
-        is >> j.pret>>j.cantitate>>j.rating>>j.cantitate;
-
+        cout<<endl<<"pret:";is >> j.pret;cout <<endl <<"cantitate:";cin>>j.cantitate;cout<<endl;cout<<"rating(intre [0-10]):";cin>>j.rating;cout<<endl<<"";
+        if (j.getRating() > 10.0)
+            j.setRating(10.0);
+        if (j.getRating() < 0.0)
+            j.setRating(0.0);
         return is;
     }
     friend std::ostream& operator<<(std::ostream &os, joc &s) {
@@ -108,30 +111,133 @@ public :
             return os;
         }
 
-        os << "Numele Jocului: " << s.name << " pret: " << s.pret << " rating: " << s.rating <<" cantitate:" <<s.cantitate <<  '\n';
+        os << "Numele Jocului: " << s.name << "; pret: " << s.pret << "; rating: " << s.rating <<"; cantitate:" <<s.cantitate <<  '\n';
+
 
         return os;
     }
-    friend class magazin;
+    friend class Magazin;
 
 };
 
 class Magazin{
 private:
+    char *name;
      unsigned  int nrJocuri;
-     joc** listaJocuri = new joc*[nrJocuri*sizeof(joc*)];
+     joc *listaJocuri ;
 public:
-    Magazin(unsigned  int nrJocuri, joc **listaJocuri){
+    Magazin() {
+        this->name = new char[1];
+        strcpy(this->name, "");
+        this->nrJocuri =0;
+        this->listaJocuri = new joc[0];
+
+    }
+
+    Magazin(char *name,unsigned  int nrJocuri, joc *listaJocuri){
             this->nrJocuri = nrJocuri;
+            this->listaJocuri = new joc[nrJocuri* sizeof(joc)];
             for(int i = 0;i <nrJocuri;i++)
                 this->listaJocuri[i] = listaJocuri[i];
+            this->name = new char[strlen(name)+1];
+            strcpy(this->name,name);
     }
     ~Magazin(){
         delete[] listaJocuri;
+        delete[] name;
     }
+
+    char *setName( const char *name_){
+        delete[] name;
+
+        int len = strlen(name_);
+        name = new char[len+1];
+        strcpy(name,name_);
+    }
+    joc *setListaJocuri( joc *listaJocuri){
+        delete[] this->listaJocuri;
+        this->listaJocuri = new joc[nrJocuri * sizeof(joc)];
+        for (int i = 0; i < this->nrJocuri; i++)
+            this->listaJocuri[i] = listaJocuri[i];
+    }
+    void setNrJocuri(const int nrJocuri){
+        this->nrJocuri = nrJocuri;
+    }
+    void adaugareJoc(joc &s) {
+        joc *listaJocuri1 = new joc[nrJocuri * sizeof(joc)];
+        for (int i = 0; i < this->nrJocuri; i++)
+            listaJocuri1[i] = this->listaJocuri[i];
+        this->nrJocuri++;
+        delete[] listaJocuri;
+        listaJocuri = new joc[nrJocuri * sizeof(joc)];
+        for (int i = 0; i < this->nrJocuri - 1; i++)
+            listaJocuri[i] = listaJocuri1[i];
+        listaJocuri1[nrJocuri - 1] = s;
+    }
+    void afisareJocuri() const{
+        for (int i = 0; i < this->nrJocuri;i++) {
+            cout << "Jocul nr " << i+1 << ':' << this->listaJocuri[i] << endl;
+        }
+    }
+    joc *getListaJocuri(){
+        return listaJocuri;
+    }
+    char *getName(){
+        return name;
+    }
+    int getNrJocuri(){
+        return nrJocuri;
+    }
+    friend std::istream& operator>>(std::istream &is, Magazin &j) {
+        cout << "Numele Mgazinului:";
+        char buf[255];
+        is >> buf;
+        j.setName(buf);
+        cout<<endl<<"nrJocuri:";is >> j.nrJocuri;cout <<endl ;
+        joc *lista = new joc[j.nrJocuri *sizeof(joc)];
+       for (int i =0;i<j.nrJocuri;i++){
+           cout << "Jocul cu nr "<<i+1<<" " <<endl;
+           is >> lista[i];
+       }
+       cout <<endl<< 100<<endl ;
+       j.setListaJocuri(lista);
+        return is;
+    }
+    friend std::ostream& operator<<(std::ostream &os, Magazin &s) {
+
+        if (!s.name) {
+            os << "Not initialized\n";
+            return os;
+        }
+
+        cout << "Numele Magazinului: "<<s.name<<"; numar de jocuri: "<<s.nrJocuri<<endl<<"Lista jocuri: "<<endl;
+        for (int i = 0; i < s.nrJocuri;i++) {
+            os << "Jocul nr " << i+1 << ':' << s.listaJocuri[i] << endl;
+        }
+
+        return os;
+    }
+
+
 };
+
+void adaugareMagazin(){
+    Magazin j1;
+    cout <<"Adaugare Magazin " << endl;
+    cin >> j1;
+    cout << "Afisare Magazin " <<endl << j1 <<endl <<endl;
+
+    cout << "Asta e tot , intoarcere la meniul principal" <<endl<<endl<<endl;
+}
+
+
+void adaugare1Joc(){
+    joc joc1;
+    cin >> joc1;
+    cout << joc1;
+    cout << "Asta e tot , intoarcere la meniul principal" <<endl<<endl<<endl;
+}
 void adaugareNJocuri(){
-    int j = 0;
     cout <<endl<<"Cate jocuri doriti sa adaugati?"<<'\n';
     int nrJocuri; cin >> nrJocuri;
     joc * listaJocuri = new joc[nrJocuri * sizeof(joc)];
@@ -141,37 +247,45 @@ void adaugareNJocuri(){
         cin >> listaJocuri[i];
 
     }
-   for (int i = 1;i <= nrJocuri ;i++)
-    {
+    for (int i = 1;i <= nrJocuri ;i++)
+        {
 
-        cout  << listaJocuri[i];
+            cout  << listaJocuri[i] <<endl;
 
-    }
+        }
+    cout << "Asta e tot , intoarcere la meniul principal" <<endl<<endl<<endl;
+
+
 }
-
-void meniuPrincipal(){
+int meniuPrincipal(){
     int optiune;
     cout<<"Alegeti optiunea pe care o doriti:" <<endl;
-    cout<<" 1.Adaugati un nou  joc."<<endl;
-    cout<<" 2.Adauga n jocuri."<<endl;
-    cout<<" 3.Oprire"<<endl;
+    cout<<" 1.Adaugati si afisati un nou  joc."<<endl;
+    cout<<" 2.Adaugati si afisati n jocuri."<<endl;
+    cout<<" 3.Adaugare magazin si afisare date."<<endl;
+    cout<<" 4.Oprire"<<endl;
     cout << "Alegeti ce optiune va doriti:";cin >> optiune;
     switch(optiune){
         case 1:
-
+            adaugare1Joc();
             break;
         case 2:
             adaugareNJocuri();
             break;
         case 3:
-
+            adaugareMagazin();
             break;
-    }
+        case 4:
+            cout << "Paaaaaaaaaaaaaaaaaaaa";
+            return 0;
 
+    }
+    return 1;
 }
 
 
+
 int main(){
-    meniuPrincipal();
+ while( meniuPrincipal());
     return 0;
 }
